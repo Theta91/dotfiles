418f024f433246176c0a35c46b3b3d7d414c18aa
Theta91 <theta91.pb@gmail.com>
2014-08-25 19:58:29 -0400

    configurable # of hours

--- scripts/bin/noise.sh
+++ scripts/bin/noise.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-play -t sl - synth 10:00:00 pinknoise band -n 1200 200 tremolo 20 .1 < /dev/zero
+# Generates pink noise for the specified number of hours. Requires sox.
 
-#sox -n --rate 48000 -b 16 35-70pinknoise.wav synth 00:00:30 pinknoise sinc 35-70 gain -n -3
-#sox -n --rate 48000 -b 16 500-1000pinknoise.flac synth 00:00:30 pinknoise sinc 500-1000 gain -n -3
+play -c 2 -r 48k -t s32 - synth ${1}:00:00 pinknoise band -n 1200 200 tremolo 20 .1 < /dev/zero
