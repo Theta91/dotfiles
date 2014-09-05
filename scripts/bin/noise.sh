8598c7befd895120a64bed80367ad1499d0f894f
Theta91 <theta91.pb@gmail.com>
2014-09-05 14:20:15 -0400

    add default value for run-time

--- scripts/bin/noise.sh
+++ scripts/bin/noise.sh
@@ -2,4 +2,4 @@
 
 # Generates pink noise for the specified number of hours. Requires sox.
 
-play -c 2 -r 48k -t s32 - synth ${1}:00:00 pinknoise band -n 1200 200 tremolo 20 .1 < /dev/zero
+play -c 2 -r 48k -t s32 - synth ${1:-8}:00:00 pinknoise band -n 1200 200 tremolo 20 .1 < /dev/zero
