2a2d4966830f65ee7cae345b0224bfd885a9cfae
Theta91 <theta91.pb@gmail.com>
2014-09-05 00:02:52 -0400

    description of ioctl-wireless-info

--- scripts/src/ioctl-wireless-info.c
+++ scripts/src/ioctl-wireless-info.c
@@ -1,4 +1,8 @@
-#include <sys/socket.h>                 /* must be before <linux/wireless.h> */
+/* A somewhat well-commented example of using ioctl to grab ESSID and quality
+ * of wireless connection. Much more is possible: read <linux/wireless.h>. */
+
+#include <sys/ioctl.h>       /* technically, not needed */
+#include <sys/socket.h>      /* must be before <linux/wireless.h> */
 #include <linux/wireless.h>
 
 #include <stdio.h>
@@ -7,11 +11,11 @@
 #define IWNAME "wlo1"
 
 int main(void) {
-    int sock;
+    int sockfd;
+    /* wstats will contain wireless statistics: quality, packets discarded/missed */
     struct iw_statistics wstats;
     /* wreq contains the union 'u.data'. We'll set its .pointer & .length anew
-     * for every ioctl call.
-     */
+     * for every ioctl call. */
     struct iwreq wreq = {
         .ifr_name = IWNAME,
     };
@@ -19,21 +23,18 @@ int main(void) {
     /* AF_INET - IP domain/address family. SOCK_DGRAM - datagram socket */
     if((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("Error getting socket");
-        return(-1);
-        }
+        return -1; }
 
     /* SIOCGIWSTATS:
      * .qual.qual - link quality
      * .qual.level - signal level (dBm)
      * .qual.noise - noise level (dBm)
-     * .qual.updated - bitmasks. important: IW_QUAL_DBM, IW_QUAL_*_UPDATED
-     */
+     * .qual.updated - bitmasks. important: IW_QUAL_DBM, IW_QUAL_*_UPDATED */
     wreq.u.data.pointer = &wstats;
     wreq.u.data.length  = sizeof(wstats);
     if(ioctl(sock, SIOCGIWSTATS, &wreq) == -1) {
         perror("Error getting stats");
-        return(-1);
-        }
+        return -1; }
 
     /* SIOCGIWESSID can be up to 32 bytes long. */
     char essid[33];
@@ -42,11 +43,10 @@ int main(void) {
     wreq.u.data.length  = sizeof(essid);
     if(ioctl(sock, SIOCGIWESSID, &wreq) == -1) {
         perror("Error getting ESSID");
-        return(-1);
-        }
+        return -1; }
 
-    printf("{ 'quality': '%d', 'essid': '%s' }\n", (wstats.qual.qual * 100)/70, essid);
+    /* max quality on my machine is 70 -- set that to 100 */
+    printf("{ 'quality': '%d', 'essid': '%s' }\n", 100 * wstats.qual.qual / 70, essid);
 
-    close(sock);
-    return 0;
-}
+    close(sockfd);
+    return 0; }
